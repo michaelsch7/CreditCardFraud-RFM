e896f2a4c79211b8859d97bd23cab8e8c6df5da8
michaelsch7 <73963133+michaelsch7@users.noreply.github.com>
2022-05-02 14:47:46 -0500

    Update randomforest-creditcardA.py

--- randomforest-creditcardA.py
+++ randomforest-creditcardA.py
@@ -15,7 +15,7 @@ from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 
-df = pd.read_csv("/Users/Michael1/Desktop/fraud-data1.csv")
+df = pd.read_csv("fraud-data1.csv")
 dfs = sqlContext.createDataFrame(df)
 
 vectorA = VectorAssembler(inputCols=['V1','V2','V3','V4','V5','V6','V7','V8','V9','V10','V11','V12','V13','V14','V15','V16','V17','V18','V19','V20', 'V21','V22','V23','V24','V25','V26','V27','V28','Amount'], outputCol = "features")
